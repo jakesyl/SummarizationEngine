7409c876f853b5e3b162a09430da00df7d314b2b
Jake Sylvestre <jakesyl@gmail.com>
2014-08-12 01:39:30 -0400

    updated try
    ‘bout to ditch wrappers

--- src/gettrends.py
+++ src/gettrends.py
@@ -1,13 +1,15 @@
 import logging
 from twitter import *
-
+from credentials import *#imports vars
 #Creates Sandy Grey, the logger
 logging.basicConfig(level=logging.INFO)#adjust level to see different levels of stuff
 logger = logging.getLogger(__name__)
 
 def trendchaser():
+	try:
+		t = Twitter(auth=OAuth(token, token_key, con_secret, con_secret_key))
+	  # Get your "home" 
 
-t = Twitter(
-    auth=OAuth(token, token_key, con_secret, con_secret_key)))
-    # Get your "home" 
-    home_timelint.statuses.home_timeline()
+	except BaseException as exc:
+		print str(exc)
+trendchaser()
