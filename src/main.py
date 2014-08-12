4bc3f6d9b100a8041dad01053ed08adc4eea6d7c
Jake Sylvestre <jakesyl@gmail.com>
2014-08-12 00:17:02 -0400

    added the main script

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import gettrends
+import logging
+'''
+Event Recognition Engine
+Author: Jake Sylvestre
+'''
+
+
+
+
+
+
+
+'''
+Engine Sudocode:
+
+Check Twitter trends
+for trends on twitter if in top 5
+put into nlp Engine
+wikipedia
+summarize summly
+twillio
+'''
+
