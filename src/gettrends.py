818b6bc0c9fe005e5f16c549505ac071bada2ccd
Jake Sylvestre <jakesyl@gmail.com>
2014-08-12 01:13:36 -0400

    added wrapper to get trends

--- src/gettrends.py
+++ src/gettrends.py
@@ -1,7 +1,13 @@
 import logging
+from twitter import *
 
 #Creates Sandy Grey, the logger
 logging.basicConfig(level=logging.INFO)#adjust level to see different levels of stuff
 logger = logging.getLogger(__name__)
 
 def trendchaser():
+
+t = Twitter(
+    auth=OAuth(token, token_key, con_secret, con_secret_key)))
+    # Get your "home" 
+    home_timelint.statuses.home_timeline()
